a4cfa6681b82db794dbe70f25fdc6622b041ed51
dingyoujian <422271637@qq.com>
2017-04-18 11:38:26 +0800

    补充提交

---
+++ src/components/Bottom/Bottom.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Bottom.scss'
+
+export const Bottom = () => (
+    <div className='index-Bottom'>
+        <span>版权所有：我自己!!!</span>
+    </div>
+)
+
+export default Bottom
---
+++ src/components/Top/Top.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Top.scss'
+
+export const Top = () => (
+    <div className='index-Top'></div>
+)
+
+export default Top
